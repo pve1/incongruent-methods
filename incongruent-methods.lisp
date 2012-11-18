2332732bef31bf780840194783486bcd1b827eff
pve1 <peterve@gmail.com>
2012-11-18 15:21:01 +0200

    Formatting.

--- incongruent-methods.lisp
+++ incongruent-methods.lisp
@@ -1,7 +1,8 @@
 (in-package :incongruent-methods)
 
 (defun method-name-with-arity (name arity)
-  (intern (format nil "~A-~A" name arity) (symbol-package name)))
+  (intern (format nil "~A-~A" name arity)
+          (symbol-package name)))
 
 (defun method-parameter-name (x)
   (etypecase x
@@ -10,6 +11,8 @@
 
 (defun method-lambda-list-arity (lambda-list)
   (length lambda-list))
+
+;;;;
 
 (defgeneric find-method-with-arity (name arity))
 
@@ -24,14 +27,19 @@
         (lambda (form env)
           (declare (ignore env))
           (if (eq (car form) 'funcall)
-              (destructuring-bind (funcall (fun-or-quote name) &rest args) form
-                `(,funcall (,fun-or-quote ,(method-name-with-arity
-                                            name
-                                            (method-lambda-list-arity args)))
+              (destructuring-bind (funcall (fun-or-quote name)
+                                           &rest args) form
+                `(,funcall (,fun-or-quote
+                            ,(method-name-with-arity
+                              name
+                              (method-lambda-list-arity args)))
                            ,@args))
               (destructuring-bind (fun &rest args) form
-                `(,(method-name-with-arity fun (method-lambda-list-arity args))
+                `(,(method-name-with-arity
+                    fun (method-lambda-list-arity args))
                    ,@args))))))
+
+;;;;
 
 (defvar *generic-arity-functions* (make-hash-table :test 'eq))
 
@@ -44,12 +52,16 @@
   (ensure-generic-function (method-name-with-arity name arity))
   (pushnew arity (gethash name *generic-arity-functions*)))
 
+;;;;
+
 (defun remove-incongruent-function (name)
   (when (incongruent-function-p name)
     (fmakunbound name)
     (dolist (arity (gethash name *generic-arity-functions*))
       (fmakunbound (method-name-with-arity name arity)))
     (remhash name *generic-arity-functions*)))
+
+;;;;
 
 (defmacro define-incongruent-method (name method-lambda-list
                                      &body body)
