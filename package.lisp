08219047b3f6b02f7792840b5fae1881e67c5ee5
pve1 <peterve@gmail.com>
2012-11-23 03:28:22 +0200

    Export symbols.

--- package.lisp
+++ package.lisp
@@ -3,5 +3,6 @@
 (defpackage :incongruent-methods
   (:use :cl)
   (:export #:define-incongruent-method
-           #:remove-incongruent-function))
-
+           #:remove-incongruent-function
+           #:define-class
+           #:define-class-method))
